95f185e642df112298b827488156cc1c14987f2c
Nikolay Stoitsev <stoitsev@uber.com>
2017-08-13 23:11:11 +0300

    Update publish script

--- publish.sh
+++ publish.sh
@@ -1,5 +1,17 @@
+#!/bin/sh
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
 hugo
 cd public
 git add .
 git commit -m "Generate site"
+git push origin master
+
+cd ..
+git add public
+git commit -m"Submodule update"
 git push origin master
